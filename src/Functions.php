8076a5c6e96cff47845002b97a63cd86e39e3c2e
Mantas <6524518+mantas6@users.noreply.github.com>
2024-12-29 12:26:53 +0200

    functional api

--- src/Functions.php
+++ src/Functions.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace FzfPhp;
 
 if (! function_exists('FzfPhp\fzf')) {
-    function fzf(array|callable $options): string
+    function fzf(array|callable|null $options): string
     {
+        if ($options === null) {
+            return new FuzzyFinder;
+        }
+
         return (new FuzzyFinder)
             ->options($options)
             ->run();
