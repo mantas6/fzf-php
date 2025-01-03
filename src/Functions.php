755582a1dffbca15b5b0d6b5789f32378084c3b7
Mantas <6524518+mantas6@users.noreply.github.com>
2025-01-03 08:54:41 +0200

    move to helper function

--- src/Functions.php
+++ src/Functions.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 namespace Mantas6\FzfPhp;
 
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Helper\TableCellStyle;
+
 if (!function_exists('FzfPhp\fzf')) {
-    /**
-     * @param  array <int, string>  $options
-     * @param  array <string, mixed>  $arguments
-     */
     function fzf($options = null, array $arguments = [], ?callable $present = null): mixed
     {
         $finder = (new FuzzyFinder)
@@ -23,5 +22,24 @@ if (!function_exists('FzfPhp\fzf')) {
         }
 
         return $finder;
+    }
+}
+
+if (!function_exists('FzfPhp\cell')) {
+    function cell(
+        string $value,
+        string $align = TableCellStyle::DEFAULT_ALIGN,
+        string $fg = 'default',
+        string $bg = 'default',
+        int $colspan = 1,
+    ): mixed {
+        return new TableCell($value, [
+            'colspan' => $colspan,
+            'style' => new TableCellStyle([
+                'align' => $align,
+                'fg' => $fg,
+                'bg' => $bg,
+            ]),
+        ]);
     }
 }
