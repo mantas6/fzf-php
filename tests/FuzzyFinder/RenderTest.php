924ea4190aca314330f92011a98d2c2e6b578731
mantas6 <6524518+mantas6@users.noreply.github.com>
2025-01-21 10:13:47 +0200

    fmt

--- tests/FuzzyFinder/RenderTest.php
+++ tests/FuzzyFinder/RenderTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Mantas6\FzfPhp\FuzzyFinder;
 use Tests\FakeProcess;
 
@@ -8,16 +10,16 @@ use function Mantas6\FzfPhp\fzf;
 
 beforeEach(fn () => FuzzyFinder::usingProcessClass(FakeProcess::class));
 
-it('renders basic options correctly', function () {
+it('renders basic options correctly', function (): void {
     fzf(['Apple', 'Orange', 'Grapefruit']);
 
     expect(FakeProcess::$lastInput)->toMatchSnapshot();
 });
 
-it('renders presented options correctly', function () {
+it('renders presented options correctly', function (): void {
     fzf(
         ['Apple', 'Orange', 'Grapefruit'],
-        present: fn (string $item) => [
+        present: fn (string $item): array => [
             $item,
             strtoupper($item),
             cell($item, fg: 'red'),
